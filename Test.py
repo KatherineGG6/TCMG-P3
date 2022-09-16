5565b0074b492848501ff1de88d47f9e5b05ed55
Katherine <katschoolwork@client-10-228-142-51.tamulink.tamu.edu>
2022-09-16 16:03:35 -0500

    AddedPrintLineText

--- Test.py
+++ Test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-#with open(r"TCMG412Lab3.txt", 'r') as fp:
-#	lines = len(fp.readlines())
-#	print('Total Number of lines:', lines)
+with open(r"TCMG412Lab3.txt", 'r') as fp:
+	lines = len(fp.readlines())
+	print('Total Number of lines:', lines)
 
 #from cgitb import lookup
 #import re
@@ -19,10 +19,10 @@
 
 file = open('TCMG412Lab3.txt','r')
 
-words = ["Oct", "Sep"]
+words = ["Oct", "Sep", "Aug", "Jul", "Jun", "May" "1995"]
 count=0
 lines=file.readlines()
 for line in lines:
     if any(word in line for word in words):
         count+=1 
-print(count)
+print('Total requests that have been made in the past 6 months: ', count)
