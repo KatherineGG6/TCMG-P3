60187543df94c4570690db4ac214117d726d7b0a
Katherine <katschoolwork@client-10-228-142-51.tamulink.tamu.edu>
2022-09-16 15:26:45 -0500

    import Counter

--- Test.py
+++ Test.py
@@ -3,3 +3,17 @@ with open(r"TCMG412Lab3.txt", 'r') as fp:
 	lines = len(fp.readlines())
 	print('Total Number of lines:', lines)
 
+from cgitb import lookup
+import re
+
+
+for search_name in ['Oct', 'Sep', 'Aug', 'Jul', 'Jun', 'May']:
+  found = lookup('TCMG412Lab3.txt', search_name)
+  print(f"Search Result for {search_name}")
+  if found:
+    name = found
+    print(f"Name: {name}")
+  else:
+    print(f"{search_name} not found")
+
+  print("")
